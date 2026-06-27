c4d991450ae7f5a082f0307f793191dd1adf78ce
philippearnault32 <philippe.arnault@outlook.com>
2026-06-27 10:24:14 +0200

    Update messages in demarrer.bat for clarity

--- demarrer.bat
+++ demarrer.bat
@@ -2,11 +2,11 @@
 title Lanceur IDE Collaboratif Automatise
 cls
 echo ===================================================
-echo    Lancement de Script - l'IDE Collaboratif Synchrone
+echo    Lancement de l'IDE Collaboratif Synchrone
 echo ===================================================
 echo.
-echo Le serveur démarre... Localtunnel et le navigateur
-echo vont s'ouvrir automatiquement d'ici quelques secondes.
+echo Le serveur démarre... 
+echo Le navigateur s'ouvrira TOUT SEUL dès que le lien sera disponible.
 echo.
 echo Laissez cette fenêtre ouverte pour garder le serveur actif.
 echo ===================================================
